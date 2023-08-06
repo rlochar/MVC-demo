fb18878b2477642a07a7a35c09caa0484a7f7e21
Roar Lochar <roarlochar@me.com>
2023-08-06 13:48:13 +0200

    Created basic setup for composing view controller

---
+++ MVC Demo/Composers/MainComposer.swift
@@ -0,0 +1,14 @@
+//
+//  MainComposer.swift
+//  MVC Demo
+//
+//  Created by Roar Lochar on 06/08/2023.
+//
+
+import Foundation
+
+class MainComposer {
+    public static func createViewController() -> ViewController {
+        return ViewController()
+    }
+}
