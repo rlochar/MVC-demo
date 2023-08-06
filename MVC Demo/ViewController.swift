76ba80e67c5b249a60495c37d60768b1babfbcf8
Roar Lochar <roarlochar@me.com>
2023-08-06 11:30:51 +0200

    Initial Commit

---
+++ MVC Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC Demo
+//
+//  Created by Roar Lochar on 06/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
